a2c12442f7cdf31dc477c97b0952a4a4f67c85b8
Abhinav KM <abhinavkm184@gmail.com>
2018-05-23 17:22:59 +0530

    some fixes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
-router.get(/\/.*/, function(req, res, next) {
-  res.redirect('/');
-});
+// router.get(/\/.*/, function(req, res, next) {
+//   res.redirect('/');
+// });
 
 module.exports = router;
